325001559423746692ace137222b1b393236efb3
Junior Moura <geraldomartins10@gmail.com>
2022-09-26 18:30:23 -0300

    commit inicial

---
+++ src/JogoDaVelha.java
@@ -0,0 +1,58 @@
+public class JogoDaVelha {
+    private int jogo[][] = new int[3][3];
+
+    public JogoDaVelha() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                jogo[i][j] = 0;
+            }
+        }
+    }
+
+    public void mostrar() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(jogo[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int verificandoVencedor() {
+        for (int i = 0; i < 3; i++) {
+            if ((jogo[i][0] == jogo[i][1]) &&
+                    (jogo[i][0] == jogo[i][2])) {
+                if (jogo[i][0] != 0) {
+                    return jogo[i][0];
+                }
+            }
+        }
+        for (int j = 0; j < 3; j++) {
+            if ((jogo[0][j] == jogo[1][j]) &&
+                    (jogo[0][j] == jogo[2][j])
+                    && (jogo[0][j] != 0)) {
+                return jogo[0][j];
+            }
+        }
+        if ((jogo[0][0] == jogo[1][1]) &&
+                (jogo[0][0] == jogo[2][2]) &&
+                (jogo[0][0] != 0)) {
+            return jogo[0][0];
+        }
+        if ((jogo[0][2] == jogo[1][1]) &&
+                (jogo[0][2] == jogo[2][0]) &&
+                (jogo[0][2] != 0)) {
+            return jogo[0][2];
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (jogo[i][j] == 0) {
+                    return 0;
+                }
+            }
+        }
+        return 3;
+    }
+
+
+}
